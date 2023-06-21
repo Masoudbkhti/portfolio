ee6925113d3089679af0e19d4f8c9a5f2c6d5a1a
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-06-21 11:49:26 +0330

    add Pills Tab to project from bootstrap

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,67 @@
-const Projects = ()=>{
-    return (
-        
-    )
-}
+import { Container, Row, Col } from "react-bootstrap";
+const Projects = () => {
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+  ];
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <h2>Projects</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti,
+              ratione saepe dolorem reprehenderit ipsam facilis eius nostrum
+              soluta facere commodi minus ipsa nobis vitae ad nisi odit alias
+              odio? Possimus.
+            </p>
+            <Nav variant="pills" defaultActiveKey="/home">
+              <Nav.Item>
+                <Nav.Link href="/home">Active</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="disabled" disabled>
+                  Disabled
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
 
 export default Projects;

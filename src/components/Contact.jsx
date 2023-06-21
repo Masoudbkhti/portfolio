65c6fa8df8469c990bea55cf2fd8e17e6bd963a2
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-06-21 13:56:25 +0330

    add contactImg and states

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import contactImg from "../assets/img/contact-img.svg";
 const Contact = () => {
-    return (
+  const forInitialDetails = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
 
-    )
-}
+  const [formDetails, setFormDetails] = useState(forInitialDetails);
+  const [buttonText, setButtonText] = useState("Send");
+  const [status, setStatus] = useState({});
+  return (
+    <section className="contact" id="contact">
+      <Container>
+        <Row className="align-items-center">
+          <Col md={6}>
+            <img src={contactImg} />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;

3f0cd8b08b3bcaaf57d266a717e89dd82ac28649
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-06-21 11:40:29 +0330

    add NavBar, Banner and Skills sections

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import NavBar from "./components/NavBar.jsx";
+import Banner from "./components/Banner.jsx";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Skills.jsx
@@ -0,0 +1,72 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { Container, Row, Col } from "react-bootstrap";
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp.png";
+
+const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 400, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <section className="skill" id="skills">
+      <Container>
+        <Row>
+          <Col>
+            <div className="skill-bx">
+              <h2>Skills</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+                nemo magnam placeat voluptas voluptatem a est magni, odio
+                recusandae nulla, officiis temporibus quidem? Similique fugiat
+                tempora laudantium odio vitae id?
+              </p>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="skill-slider"
+              >
+                <div className="item">
+                  <img src={meter1} alt="image" />
+                  <h5>Web Development</h5>
+                </div>
+                <div className="item">
+                  <img src={meter2} alt="image" />
+                  <h5>Brand Identity</h5>
+                </div>
+                <div className="item">
+                  <img src={meter3} alt="image" />
+                  <h5>Logo Design</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="image" />
+                  <h5>Web Development</h5>
+                </div>
+              </Carousel>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-left" src={colorSharp} />
+    </section>
+  );
+};
+export default Skills;

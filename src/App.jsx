b3ff9a6a4a2af16ef725f68f9d213a46a22b4f48
Masoud Bakhtiari <masoudbkhti@gmail.com>
2023-06-24 09:13:37 +0330

    App width set to 100%

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 function App() {
   return (
-    <div>
+    <div className="App">
       <NavBar />
       <Banner />
       <Skills />
